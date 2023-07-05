3b3ac5a1a9b293c77644ed74b0b2edd29d623d00
Quoc Thanh <thanhboks128@gmail.com>
2023-07-05 22:58:01 +0700

    demo

---
+++ quan_ly_sv/ListSV.java
@@ -0,0 +1,71 @@
+package quan_ly_sv;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class ListSV {
+    public static void main(String[] args){
+        ArrayList<Student> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do{
+            System.out.println("1. Them sinh vien.");
+            System.out.println("2. Hien thi danh sach SV.");
+            System.out.println("3. Cap nhat thong tin SV.");
+            System.out.println("4. Xoa thong tin SV.");
+            System.out.println("Nhap 0 de thoat chuong trinh.");
+
+            choice=sc.nextInt();
+            if(choice==1){
+                Student s = new Student();
+                s.input();
+                list.add(s);
+                System.out.println("Da them sinh vien "+s.getId()+"\n");
+            }
+            else if(choice==2){
+                System.out.println("MSSV - ten - tuoi - chuyen nganh");
+                for(Student s:list){
+                    s.output();
+                }
+                System.out.println("\n");
+            }
+            else if(choice==3){
+                System.out.print("Nhap MSSV cua sinh vien muon cap nhat: ");
+                int ID=sc.nextInt();
+                sc.nextLine();
+                int i=0;
+                boolean a=false;
+                for(;i<list.size();i++){
+                    Student s=list.get(i);
+                    if(s.getId()==ID){
+                        a=true;
+                        break;
+                    }
+                }
+                if(a){
+                    System.out.println("Cap nhat thong tin");
+                    Student s=list.get(i);
+                    s.input();
+                    System.out.println("Da cap nhat thong tin sinh vien "+s.getId()+"\n");
+                }
+                else{
+                    System.out.println("Khong tim thay sinh vien "+ID+"\n");
+                }
+            }
+            else if(choice==4){
+                System.out.print("Nhap MSSV cua sinh vien muon xoa: ");
+                int ID=sc.nextInt();
+                sc.nextLine();
+                int i=0;
+                for(;i<list.size();i++){
+                    Student s=list.get(i);
+                    if(s.getId()==ID){
+                        list.remove(i);
+                        i=-1;
+                        System.out.println("Da xoa sinh vien "+ID+"\n");
+                        break;
+                    }
+                }
+                if(i>=0) System.out.println("Khong tim thay sinh vien "+ID+"\n");
+            }
+        }while(choice==1||choice==2||choice==3||choice==4);
+    }
+}
---
+++ quan_ly_sv/Student.java
@@ -0,0 +1,51 @@
+package quan_ly_sv;
+import java.util.Scanner;
+public class Student {
+    public Student(){}
+    private int id,age;
+    private String name,major;
+    Scanner sc=new Scanner(System.in);
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    public void input(){
+        System.out.print("Nhap MSSV: ");
+        setId(sc.nextInt());
+        sc.nextLine();
+        System.out.print("Nhap ten SV: ");
+        setName(sc.nextLine());
+        System.out.print("Nhap tuoi SV: ");
+        setAge(sc.nextInt());
+        sc.nextLine();
+        System.out.print("Nhap chuyen nganh SV: ");
+        setMajor(sc.nextLine());        
+    }
+    public void output(){
+        System.out.print(getId()+" - ");
+        System.out.print(getName()+" - ");
+        System.out.print(getAge()+" - ");
+        System.out.println(getMajor());
+    }
+    
+}
